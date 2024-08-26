1b7ebd3f30316e18e265b88d06111e49ef9d55c0
Vruz1103 <vruz2001@gmail.com>
2024-08-27 02:37:30 +0530

    added learnings of the reference videos

---
+++ first.js
@@ -0,0 +1,364 @@
+// reference videos link-https://www.youtube.com/watch?v=ajdRvxDWH4w&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=1
+//                       https://www.youtube.com/watch?v=Zg4-uSjxosE&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=2
+//                       https://www.youtube.com/watch?v=UmRtFFSDSFo&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=3
+//                       https://www.youtube.com/watch?v=gFWhbjzowrM&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=4
+
+// console.log("Vrunda Raval");
+// console.log("hello");
+// console.log("I love myself");
+
+// name = "Vrunda Raval";
+// console.log(name);
+
+// fullName = "Tony Stark";
+// age = 24;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// console.log(fullName);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(y);
+// isFollow = false;
+// console.log(isFollow);
+// fullName = 25;
+// console.log(fullName);
+
+// let myAge = 65;
+// myAge = 59;
+// console.log(myAge);
+
+// var herAge = 57;
+// var herAge = 47;
+// console.log(herAge);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// const student = {
+//   fullName: "Prince",
+//   age: 24,
+//   cgpa: 8.5,
+//   isPass: true,
+// };
+// student["age"] = student["age"] + 1;
+
+// console.log(student["age"]);
+// console.log(student.age);
+
+// const prod = {
+//   title: "Ball Pen",
+//   rating: 4,
+//   offer: 5,
+//   price: 270,
+// };
+// console.log(prod);
+
+// const prof = {
+//   username: "@vruzyyy",
+//   isFollow: true,
+//   followers: 123,
+//   following: 456,
+// };
+
+// console.log(prof);
+
+// console.log("Hello World");
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a, "b=", b);
+// console.log("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a*b=", a * b);
+// console.log("a/b=", a / b);
+// console.log("a%b=", a % b);
+// console.log("a**=", a ** b);
+// a = a + 1;
+// console.log(a);
+// a = a++;
+// console.log(a);
+// b = b - 1;
+// console.log(b);
+// b = b--;
+// console.log(b);
+// a = ++a;
+// console.log(a);
+// b = --b;
+// console.log(b);
+
+// a += 4;
+// console.log(a);
+// a -= 4;
+// console.log(a);
+// a *= 4;
+// console.log(a);
+// a /= 4;
+// console.log(a);
+// a %= 4;
+// console.log(a);
+// a **= 4;
+// console.log(a);
+
+// console.log("a==b", a == b);
+// console.log("a===b", a === b);
+// console.log("a=b", (a = b));
+// console.log("a!=b", a != b);
+
+// console.log("a>b", a > b);
+// console.log("a<b", a < b);
+// console.log("a>=b", a >= b);
+// console.log("a<=b", a <= b);
+
+// let cond1 = a > b;
+// let cond2 = a === 5;
+// console.log("cond1 && cond2", cond1 && cond2);
+
+// let cond3 = a > b;
+// let cond4 = a === 6;
+// console.log("cond3 && cond4", cond3 && cond4);
+
+// console.log("cond1 || cond2", cond1 || cond2);
+
+// console.log("cond3 || cond4", cond3 || cond4);
+
+// console.log("!(cond1 && cond2)", !(cond1 && cond2));
+// console.log("!(cond3 && cond4)", !(cond3 && cond4));
+// console.log("!(cond1 || cond2)", !(cond1 || cond2));
+// console.log("!(cond3 || cond4)", !(cond3 || cond4));
+// let age = 25;
+// if (age > 18) {
+//   console.log("You can vote");
+// }
+
+// let mode = "dark";
+// let color;
+// if (mode === "dark") {
+//   color = "black";
+// } else if (mode === "blue") {
+//   color = "blue";
+// } else {
+//   color = "white";
+// }
+// if (mode === "light") {
+//   color = "white";
+// }
+// console.log(color);
+
+// let price = 50;
+// let value = price > 30 ? console.log("expensive") : console.log("cheap");
+
+// alert("Hi Vrunda");
+
+// prompt("Hello");
+
+// let askName = prompt("Enter Name");
+// console.log("askName");
+
+// let num = prompt("Enter number");
+// if (num % 5 === 0) {
+//   console.log("Multiple of 5");
+// } else {
+//   console.log("Not a multiple of 5");
+// }
+
+// let marks= prompt("Enter marks");
+// if(marks>=80&& marks<=100){
+//     console.log("A")
+// }else if(marks>=70&& marks<=89){
+//  console.log("B")
+// }else if(marks>=60&& marks<=79){
+//     console.log("C")
+// }else if(marks>=50&& marks<=59){
+//     console.log("D")
+// }else if(marks>=0&& marks<=49){
+//     console.log("F")
+// }else{
+//     console.log("Better luck next time");
+// }
+
+// for (let c = 1; c <= 5; c++) {
+//   console.log("Hi");
+// }
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//   sum = sum + i;
+// }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("i=", i);
+//   i++;
+// }
+
+// let k = 20;
+// do {
+//   console.log("Vrunda Raval");
+//   i++;
+// } while (i <= 10);
+
+// let str = "RavalFamily";
+// for (let i of str) {
+//   console.log("i=", i);
+// }
+
+// let student = {
+//   name: "Rahul Sharma",
+//   age: 20,
+//   cgpa: 6.8,
+//   isPass: true,
+// };
+// for (let i in student) {
+//   console.log("key=", i, "value=", student[i]);
+// }
+
+// for (let num = 0; num <= 100; num++) {
+//   console.log("num=", num);
+//   if (num % 2 === 0) {
+//     console.log("num=", num);
+//   }
+//   if (num % 2 != 0) {
+//     console.log("num=", num);
+//   }
+// }
+
+// let ask = prompt("Enter number");
+// let gameNum = 25;
+
+// for(let num=0;num<=;num++){
+//   if(num===gameNum){
+//     console.log("You Guessed it right");
+//   }
+//   else{
+//     console.log("Please try again");
+//   }
+// }
+
+// while (ask != gameNum) {
+//   ask = prompt("Please try again");
+// }
+// console.log("You Guessed it correct");
+
+// let str = "Vrunda Raval";
+// let str2 = "Vrunda";
+
+// console.log(str2.length);
+// console.log(str[2]);
+
+// let sentence = `This is a log`;
+// console.log(sentence);
+// console.log(typeof sentence);
+
+// let student = {
+//   sname: "Priyam",
+//   sage: 23,
+// };
+// let collegeName = `He is a Sal college student`;
+
+// console.log(student.sname, "is a bright student and his age is", student.sage);
+// console.log(
+//   `${student.sname} is a very bright student and his age is ${student.sage}`
+// );
+// let random = `${student.sname} is a very bright student and his age is ${student.sage}`;
+// console.log(random);
+
+// let sum = `it is ${1 + 3 + 5}`;
+// console.log(sum);
+
+// console.log("Vrunda\nRaval");
+
+// let str = "gls College";
+// console.log(str);
+// console.log(str.toUpperCase());
+// let str2 = "SAL";
+// console.log(str2.toLowerCase());
+// let str3 = "    jay shree krishna    ";
+// console.log(str3.trim()); //removes space from starting and end only.
+// let str4 = "abcdefghi";
+// console.log(str.slice(3, 5));
+// console.log(str.concat(str2));
+// console.log(str4.charAt(5));
+// console.log(str3.replace("j", "m"));
+// console.log(str3.replaceAll("e", "i"));
+
+// let askUser = prompt("Enter your name for creating userid");
+
+// let prefix = "@";
+// let suffix = askUser.length;
+// let userId = `${prefix}${askUser}${suffix}`;
+// console.log(userId);
+
+// let marks_student1 = 97;
+// let marks_student = 54;
+
+// let marks = {
+//   stu1: 97,
+//   stu2: 54,
+// };
+
+// let marksArray = [97, 54, 21];
+// console.log(marksArray);
+// console.log(marksArray.length);
+// console.log(marksArray[2]);
+// marksArray[2] = 87;
+// console.log(marksArray[2]);
+
+// let cars = ["Lamborghini", "Ferrari", "Volvo", "Mercedes", "Buggati"];
+// for (let num = 0; num < cars.length; num++) {
+//   console.log(cars[num]);
+// }
+
+// for (let car of cars) {
+//   console.log(car.toUpperCase());
+// }
+
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let mark of marks) {
+//   sum = sum + mark;
+// }
+// let avg = sum / marks.length;
+// console.log(avg);
+// console.log(sum);
+
+// let itemsPrice = [250, 640, 300, 900, 50];
+
+// let c = 0;
+// for (let ip of itemsPrice) {
+//   console.log(ip);
+//   let offer = ip / 10;
+//   itemsPrice[c] = itemsPrice[c] - offer;
+//   console.log(itemsPrice[c]);
+//   c++;
+// }
+
+// let foodItems = ["spinach", "corn", "potato", "tomato", "grapes", "apple"];
+// let fastFood = ["piiza", "burger", "noodles", "waffle"];
+
+// foodItems.push("chips");
+// console.log(foodItems);
+
+// foodItems.pop();
+// console.log(foodItems);
+// console.log(foodItems.toString());
+
+// let newArray = foodItems.concat(fastFood);
+// console.log(newArray);
+// fastFood.unshift("pasta");
+// console.log(fastFood);
+// let val = fastFood.shift();
+// console.log(val);
+
+// console.log(foodItems.slice(1, 3));
+// console.log(foodItems.splice(1, 2, "bottlegaurd"));
+// console.log(foodItems);
+
+let companies = ["bloomberg", "microsoft", "uber", "google", "IBM", "netflix"];
+
+console.log(companies.shift());
+console.log(companies);
+console.log(companies.splice(2, 1, "Ola"));
+console.log(companies);
+console.log(companies.push("Amazon"));
+console.log(companies);
